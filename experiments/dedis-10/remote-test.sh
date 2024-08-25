93ee336e37832e4fdf1a4b6c28bde2dfed09f506
Pasindu Tennage <pasindunivanthakatennage@gmail.com>
2024-08-25 21:38:51 +0200

    checked remote-test.sh

--- experiments/dedis-10/remote-test.sh
+++ experiments/dedis-10/remote-test.sh
@@ -7,42 +7,49 @@ iteration=$5
 pwd=$(pwd)
 . "${pwd}"/experiments/dedis-10/ip.sh
 
-python3 experiments/python/genrate-configs.py --wave_length "${wave_length}" --number_of_leaders "${number_of_leaders}" --enable_pipelining "${enable_pipelining}" --consensus_only "${consensus_only}" --enable_synchronizer "${enable_synchronizer}" --initial_delay_secs "${initial_delay_secs}" --initial_delay_nanos "${initial_delay_nanos}" --load "${load}" --transaction_size "${transaction_size}" --output_dir "logs/dedis-10/"
+python3 experiments/python/genrate-configs.py  --timeout_delay ${timeout_delay_ms} --min_block_delay ${min_block_delay_ms} --replicas ${replica1_name} ${replica2_name} ${replica3_name} ${replica4_name} ${replica5_name}  --output_path logs/dedis-10/
 
-remote_home_path="/home/${username}/a_mysticeti/"
-kill_instances="pkill -f mysticeti ; pkill -f mysticeti"
-remote_replica_path="/a_mysticeti/mysticeti"
+nodes="${replica1_name}:10000 ${replica2_name}:10000 ${replica3_name}:10000 ${replica4_name}:10000 ${replica5_name}:10000"
+
+remote_home_path="/home/${username}/hotstuff-3-chain/"
+kill_instances="pkill -f node ; pkill -f client"
+remote_replica_path="/hotstuff-3-chain/node"
+remote_client_path="/hotstuff-3-chain/client"
 
 
 for index in "${!replicas[@]}";
 do
     echo "copying configs to replica ${index}"
-    scp -i ${cert} logs/dedis-10/node-parameters.yml   "${replicas[${index}]}":"${remote_home_path}"
-    scp -i ${cert} logs/dedis-10/client-parameters.yml "${replicas[${index}]}":"${remote_home_path}"
-    sshpass ssh "${replicas[${index}]}" -i ${cert} "${kill_instances}"
-    sshpass ssh "${replicas[${index}]}" -i ${cert} "rm ${remote_home_path}storage-${index}/wal"
-    sshpass ssh "${replicas[${index}]}" -i ${cert} ".${remote_replica_path} benchmark-genesis --ips ${replica1_name}  ${replica2_name} ${replica3_name} ${replica4_name} ${replica5_name} ${replica6_name} ${replica7_name} ${replica8_name} ${replica9_name} ${replica10_name} --working-directory ${remote_home_path} --node-parameters-path ${remote_home_path}node-parameters.yml"
+    scp -i ${cert} logs/dedis-10/.committee.json   "${replicas[${index}]}":"${remote_home_path}"
+    scp -i ${cert} logs/dedis-10/.parameters.json   "${replicas[${index}]}":"${remote_home_path}"
+    scp -i ${cert} logs/dedis-10/.node-{index}.json   "${replicas[${index}]}":"${remote_home_path}"
+
+    sshpass ssh "${replicas[${index}]}" -i ${cert} "${kill_instances}; rm -r ${remote_home_path}/logs; mkdir -p ${remote_home_path}/logs"
+    sshpass ssh "${replicas[${index}]}" -i ${cert} "${kill_instances}; rm ${remote_home_path}/.db-${index}"
 done
 
 sleep 5
 rm nohup.out
 
-local_output_path="logs/dedis-10/${wave_length}/${number_of_leaders}/pipelining-${enable_pipelining}/synchronizer-${enable_synchronizer}/${load}/${transaction_size}/${iteration}/"
+local_output_path="logs/dedis-10/${timeout_delay_ms}/${min_block_delay_ms}/${transaction_size}/${load}/${iteration}/"
 
 rm -r "${local_output_path}"; mkdir -p "${local_output_path}"
 
 echo "starting replicas"
 
-nohup ssh "${replica1}"   -i ${cert}   ".${remote_replica_path} run --authority 0 --committee-path ${remote_home_path}committee.yaml --public-config-path ${remote_home_path}public-config.yaml --private-config-path ${remote_home_path}private-config-0.yaml --client-parameters-path ${remote_home_path}client-parameters.yml" >${local_output_path}v0.log.ansi &
-nohup ssh "${replica2}"   -i ${cert}   ".${remote_replica_path} run --authority 1 --committee-path ${remote_home_path}committee.yaml --public-config-path ${remote_home_path}public-config.yaml --private-config-path ${remote_home_path}private-config-1.yaml --client-parameters-path ${remote_home_path}client-parameters.yml" >${local_output_path}v1.log.ansi &
-nohup ssh "${replica3}"   -i ${cert}   ".${remote_replica_path} run --authority 2 --committee-path ${remote_home_path}committee.yaml --public-config-path ${remote_home_path}public-config.yaml --private-config-path ${remote_home_path}private-config-2.yaml --client-parameters-path ${remote_home_path}client-parameters.yml" >${local_output_path}v2.log.ansi &
-nohup ssh "${replica4}"   -i ${cert}   ".${remote_replica_path} run --authority 3 --committee-path ${remote_home_path}committee.yaml --public-config-path ${remote_home_path}public-config.yaml --private-config-path ${remote_home_path}private-config-3.yaml --client-parameters-path ${remote_home_path}client-parameters.yml" >${local_output_path}v3.log.ansi &
-nohup ssh "${replica5}"   -i ${cert}   ".${remote_replica_path} run --authority 4 --committee-path ${remote_home_path}committee.yaml --public-config-path ${remote_home_path}public-config.yaml --private-config-path ${remote_home_path}private-config-4.yaml --client-parameters-path ${remote_home_path}client-parameters.yml" >${local_output_path}v4.log.ansi &
-nohup ssh "${replica6}"   -i ${cert}   ".${remote_replica_path} run --authority 5 --committee-path ${remote_home_path}committee.yaml --public-config-path ${remote_home_path}public-config.yaml --private-config-path ${remote_home_path}private-config-5.yaml --client-parameters-path ${remote_home_path}client-parameters.yml" >${local_output_path}v5.log.ansi &
-nohup ssh "${replica7}"   -i ${cert}   ".${remote_replica_path} run --authority 6 --committee-path ${remote_home_path}committee.yaml --public-config-path ${remote_home_path}public-config.yaml --private-config-path ${remote_home_path}private-config-6.yaml --client-parameters-path ${remote_home_path}client-parameters.yml" >${local_output_path}v6.log.ansi &
-nohup ssh "${replica8}"   -i ${cert}   ".${remote_replica_path} run --authority 7 --committee-path ${remote_home_path}committee.yaml --public-config-path ${remote_home_path}public-config.yaml --private-config-path ${remote_home_path}private-config-7.yaml --client-parameters-path ${remote_home_path}client-parameters.yml" >${local_output_path}v7.log.ansi &
-nohup ssh "${replica9}"   -i ${cert}   ".${remote_replica_path} run --authority 8 --committee-path ${remote_home_path}committee.yaml --public-config-path ${remote_home_path}public-config.yaml --private-config-path ${remote_home_path}private-config-8.yaml --client-parameters-path ${remote_home_path}client-parameters.yml" >${local_output_path}v8.log.ansi &
-nohup ssh "${replica10}"  -i ${cert}   ".${remote_replica_path} run --authority 9 --committee-path ${remote_home_path}committee.yaml --public-config-path ${remote_home_path}public-config.yaml --private-config-path ${remote_home_path}private-config-9.yaml --client-parameters-path ${remote_home_path}client-parameters.yml" >${local_output_path}v9.log.ansi &
+nohup ssh "${replica1}"    -i ${cert}   ".${remote_replica_path}  run --keys ${remote_replica_path}.node-0.json --committee ${remote_replica_path}.committee.json --store ${remote_replica_path}.db-0  --parameters ${remote_replica_path}.parameters.json" >${local_output_path}0.log &
+nohup ssh "${replica2}"    -i ${cert}   ".${remote_replica_path}  run --keys ${remote_replica_path}.node-1.json --committee ${remote_replica_path}.committee.json --store ${remote_replica_path}.db-1  --parameters ${remote_replica_path}.parameters.json" >${local_output_path}1.log &
+nohup ssh "${replica3}"    -i ${cert}   ".${remote_replica_path}  run --keys ${remote_replica_path}.node-2.json --committee ${remote_replica_path}.committee.json --store ${remote_replica_path}.db-2  --parameters ${remote_replica_path}.parameters.json" >${local_output_path}2.log &
+nohup ssh "${replica4}"    -i ${cert}   ".${remote_replica_path}  run --keys ${remote_replica_path}.node-3.json --committee ${remote_replica_path}.committee.json --store ${remote_replica_path}.db-3  --parameters ${remote_replica_path}.parameters.json" >${local_output_path}3.log &
+nohup ssh "${replica5}"    -i ${cert}   ".${remote_replica_path}  run --keys ${remote_replica_path}.node-4.json --committee ${remote_replica_path}.committee.json --store ${remote_replica_path}.db-4  --parameters ${remote_replica_path}.parameters.json" >${local_output_path}4.log &
+
+sleep 10
+
+nohup ssh "${replica6}"      -i ${cert}   ".${remote_client_path} ${replica1_name}:10000 --size ${transaction_size} --rate ${load} --timeout 3000 ${nodes}" >${local_output_path}5.log &
+nohup ssh "${replica7}"      -i ${cert}   ".${remote_client_path} ${replica2_name}:10000 --size ${transaction_size} --rate ${load} --timeout 3000 ${nodes}" >${local_output_path}6.log &
+nohup ssh "${replica8}"      -i ${cert}   ".${remote_client_path} ${replica3_name}:10000 --size ${transaction_size} --rate ${load} --timeout 3000 ${nodes}" >${local_output_path}7.log &
+nohup ssh "${replica9}"      -i ${cert}   ".${remote_client_path} ${replica4_name}:10000 --size ${transaction_size} --rate ${load} --timeout 3000 ${nodes}" >${local_output_path}8.log &
+nohup ssh "${replica110}"    -i ${cert}   ".${remote_client_path} ${replica5_name}:10000 --size ${transaction_size} --rate ${load} --timeout 3000 ${nodes}" >${local_output_path}9.log &
 
 
 sleep 120
@@ -52,29 +59,3 @@ do
     echo "killing instance"
     sshpass ssh "${replicas[${index}]}" -i ${cert} "${kill_instances}"
 done
-
-home_path="/home/${username}/"
-
-scp -i ${cert} ${replica1}:${home_path}client-times-0.txt  ${local_output_path}client-times-0.txt
-scp -i ${cert} ${replica2}:${home_path}client-times-1.txt  ${local_output_path}client-times-1.txt
-scp -i ${cert} ${replica3}:${home_path}client-times-2.txt  ${local_output_path}client-times-2.txt
-scp -i ${cert} ${replica4}:${home_path}client-times-3.txt  ${local_output_path}client-times-3.txt
-scp -i ${cert} ${replica5}:${home_path}client-times-4.txt  ${local_output_path}client-times-4.txt
-scp -i ${cert} ${replica6}:${home_path}client-times-5.txt  ${local_output_path}client-times-5.txt
-scp -i ${cert} ${replica7}:${home_path}client-times-6.txt  ${local_output_path}client-times-6.txt
-scp -i ${cert} ${replica8}:${home_path}client-times-7.txt  ${local_output_path}client-times-7.txt
-scp -i ${cert} ${replica9}:${home_path}client-times-8.txt  ${local_output_path}client-times-8.txt
-scp -i ${cert} ${replica10}:${home_path}client-times-9.txt ${local_output_path}client-times-9.txt
-
-output_file=${local_output_path}output.txt
-
-python3 experiments/python/client-stats.py ${local_output_path}client-times-0.txt ${initial_delay_secs} 0 >> ${output_file}
-python3 experiments/python/client-stats.py ${local_output_path}client-times-1.txt ${initial_delay_secs} 1 >> ${output_file}
-python3 experiments/python/client-stats.py ${local_output_path}client-times-2.txt ${initial_delay_secs} 2 >> ${output_file}
-python3 experiments/python/client-stats.py ${local_output_path}client-times-3.txt ${initial_delay_secs} 3 >> ${output_file}
-python3 experiments/python/client-stats.py ${local_output_path}client-times-4.txt ${initial_delay_secs} 4 >> ${output_file}
-python3 experiments/python/client-stats.py ${local_output_path}client-times-5.txt ${initial_delay_secs} 5 >> ${output_file}
-python3 experiments/python/client-stats.py ${local_output_path}client-times-6.txt ${initial_delay_secs} 6 >> ${output_file}
-python3 experiments/python/client-stats.py ${local_output_path}client-times-7.txt ${initial_delay_secs} 7 >> ${output_file}
-python3 experiments/python/client-stats.py ${local_output_path}client-times-8.txt ${initial_delay_secs} 8 >> ${output_file}
-python3 experiments/python/client-stats.py ${local_output_path}client-times-9.txt ${initial_delay_secs} 9 >> ${output_file}
