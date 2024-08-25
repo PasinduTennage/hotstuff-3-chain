0ee187230ae4405871b088bf7ae2cc6ac5344706
Pasindu Tennage <pasindunivanthakatennage@gmail.com>
2024-08-25 20:18:25 +0200

    checked setup.sh

--- experiments/dedis-10/setup.sh
+++ experiments/dedis-10/setup.sh
@@ -5,19 +5,22 @@ rm -r logs/dedis-10/ ; mkdir -p logs/dedis-10/
 
 remote_home_path="/home/${username}/hotstuff-3-chain/"
 reset_directory="sudo rm -r ${remote_home_path}; mkdir -p ${remote_home_path}logs/"
-kill_instances="pkill -f node ; pkill -f node"
+kill_instances="pkill -f node ; pkill -f client"
 install_dependencies="sudo apt-get update"
 
 # build the binary in a remote replica, uncomment when needed
 sshpass ssh "${replicas[0]}" -i ${cert} "rm -r ${remote_home_path}; git clone https://github.com/PasinduTennage/hotstuff-3-chain; cd hotstuff-3-chain; git checkout 3-chain; sudo apt-get install libfontconfig1-dev; source $HOME/.cargo/env; cargo build --release;"
+#sshpass ssh "${replicas[0]}" -i ${cert} "cd hotstuff-3-chain; git checkout 3-chain; git pull origin 3-chain; sudo apt-get install libfontconfig1-dev; source $HOME/.cargo/env; cargo build --release;"
 scp -i ${cert} "${replicas[0]}":${remote_home_path}target/release/node logs/dedis-10/node
+scp -i ${cert} "${replicas[0]}":${remote_home_path}target/release/client logs/dedis-10/client
 
 
 for index in "${!replicas[@]}";
 do
     echo "copying files to replica ${index}"
     sshpass ssh "${replicas[${index}]}" -i ${cert} "${reset_directory};${kill_instances};${install_dependencies}"
-    scp -i ${cert} logs/dedis-10/node "${replicas[${index}]}":${remote_home_path} # first download from replica 0
+    scp -i ${cert} logs/dedis-10/node "${replicas[${index}]}":${remote_home_path}
+    scp -i ${cert} logs/dedis-10/client "${replicas[${index}]}":${remote_home_path}
 done
 
 echo "setup complete"
