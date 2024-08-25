ed73a148bb46f3b2f910d69821b39c8f7f8e6e92
Pasindu Tennage <pasindunivanthakatennage@gmail.com>
2024-08-25 20:34:56 +0200

    generate-configs checked

--- experiments/python/genrate-configs.py
+++ experiments/python/genrate-configs.py
@@ -2,15 +2,13 @@ import os
 import argparse
 import json
 
-# this file takes as inputs timeout_delay, min_block_delay, replica ips and generate 1) private configs 2) .parameters.json, 3) .committee.json
-
 # Set up command-line argument parsing
-parser = argparse.ArgumentParser(description='Generate node-parameters.yml and client-parameters.yml files.')
+parser = argparse.ArgumentParser()
 
 parser.add_argument('--timeout_delay', type=int, default=30000, help='Hotstuff timeout (default: 30s)')
 parser.add_argument('--min_block_delay', type=int, default=100, help='Minimum block delay (default: 100ms)')
 parser.add_argument('--replicas', type=str, default="", nargs='+', help='Replicas IPs')
-parser.add_argument('--output_path', type=str, default="logs/dedis-10/", nargs='+', help='output dir')
+parser.add_argument('--output_path', type=str, default="logs/dedis-10/", help='output dir')
 
 args = parser.parse_args()
 
@@ -30,21 +28,17 @@ node_parameters_dict = {
     }
 }
 
-############
-# params
-
 with open(args.output_path + '.parameters.json', 'w') as json_file:
     json.dump(node_parameters_dict, json_file, indent=4)
 
 ############
 # key files
 for i in range(len(args.replicas)):
-    os.system(f".{args.output_path}node keys --filename {args.output_path}.node-{i}.json")
+    os.system(f"./{args.output_path}node keys --filename {args.output_path}.node-{i}.json")
 
 
 ##########
 # committee file
-
 
 def load_from_file(filename):
     with open(filename, 'r') as f:
@@ -54,33 +48,32 @@ def load_from_file(filename):
 
 keys = []
 key_files = [f"{args.output_path}.node-{i}.json" for i in range(len(args.replicas))]
-
 for filename in key_files:
     keys += [load_from_file(filename)]
 
-names = [x.name for x in keys]
+names = [x[0] for x in keys]
 consensus_addr = [f'{x}:{9000}' for x in args.replicas]
 front_addr = [f'{x}:{10000}' for x in args.replicas]
 mempool_addr = [f'{x}:{11000}' for x in args.replicas]
 
 
-def _build_consensus(consensus, names):
+def _build_consensus(_consensus, _names):
     node = {}
-    for a, n in zip(consensus, names):
+    for a, n in zip(_consensus, _names):
         node[n] = {'name': n, 'stake': 1, 'address': a}
     return {'authorities': node, 'epoch': 1}
 
 
-def _build_mempool(names, front, mempool):
+def _build_mempool(_names, _front, _mempool):
     node = {}
-    for n, f, m in zip(names, front, mempool):
+    for n, f, m in zip(names, _front, _mempool):
         node[n] = {'name': n, 'front_address': f, 'mempool_address': m}
     return {'authorities': node, 'epoch': 1}
 
 
 cmt = {
-    'consensus': _build_consensus(),
-    'mempool': _build_mempool()
+    'consensus': _build_consensus(consensus_addr, names),
+    'mempool': _build_mempool(names, front_addr, mempool_addr)
 }
 
 with open(args.output_path + ".committee.json", 'w') as f:
